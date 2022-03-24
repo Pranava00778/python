e3ad1c375943429d6731ed58e0cb9518148d3fea
v2webtest <50831098+v2webtest@users.noreply.github.com>
2022-03-24 23:47:07 +0530

    List Tuple Dictionary

---
+++ list-tuples.py
@@ -0,0 +1,10 @@
+lstStudents=["Anil","Suresh","Ramesh","Dinesh"]
+print(lstStudents)
+print(lstStudents.__sizeof__())
+tplStudents=("Anil","Suresh","Ramesh","Dinesh")
+print(tplStudents)
+print(tplStudents.__sizeof__())
+
+# Tuples takes less space because they are written as whole block
+# Tuples are faster to access
+# Tuples starts with ( and ends with )
---
+++ maps-dictionary.py
@@ -0,0 +1,10 @@
+# Collection of Name value pairs
+golu ={"Name":"Pranava", "Age":"13","Class":"VIII"}
+print(golu["Class"])
+
+# Dictionary of Dictionary
+dctStudents={
+    "Golu":{"Name":"Pranava", "Age":"13","Class":"VIII"},
+    "Adi":{"Name":"Aaditya", "Age":"19","Class":"XIII"}
+    }
+print(dctStudents["Golu"]["Name"])
