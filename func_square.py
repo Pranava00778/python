36c7a1e9cef9e933e7003c12d89fcf24ef7976fa
Pranava00778 <103672656+Pranava00778@users.noreply.github.com>
2022-04-15 23:11:45 -0700

    New UpdaTES

---
+++ func_square.py
@@ -0,0 +1,20 @@
+import turtle
+t = turtle.Pen()
+
+def mySquare(size):
+    for i in range
+    888888111111111111111111111!~````````````
+t.reset()
+
+#mySquare(50)
+# mySquare(100)
+# mySquare(150)
+# mySquare(200)
+# mySquare(250)
+
+for i in range(250,10,-10):
+    mySquare(i)
+
+
+
+
